81b583870dd92f348bb6b11d0839c3afd8848c6a
ashish095-EE <127418703+ashish095-EE@users.noreply.github.com>
2024-07-11 03:40:07 +0530

    created signup api,error handler,middlewear setup

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.routes.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -12,11 +12,27 @@ mongoose.connect(process.env.MONGO).then(() => {
     
 });
 
+
+
 const app = express();
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message ||"Intenal Server Error";
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+});
+
 
 app.listen(3000, () => {
     console.log("server running on port 3000");
 });
 
-app.use('/api/user', userRoutes);
 
