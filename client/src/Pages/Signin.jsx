50345c09660c5392880a0a1835510f203fe2753c
ashish095-EE <127418703+ashish095-EE@users.noreply.github.com>
2024-08-06 20:12:31 +0530

    redux toolkit on sigin page

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,53 +1,45 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
-  const [formdata, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState('');
-  const [isLoading, setLoading] = useState(false);
+  const [formdata, setFormData] = useState({ email: '', password: '' });
+  const { loading, error: errorMessage } = useSelector(state => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formdata, [e.target.id]: e.target.value.trim() });
-    console.log(formdata);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault(); // prevent default browser behavior of refreshing on submit.
 
     if (!formdata.password || !formdata.email) {
-      return setErrorMessage('Please fill out all the fields');
+      return dispatch(signInFailure('Please fill out all the fields! '));
     }
 
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formdata),
       });
       const data = await res.json();
-      if(data.sucess===false){
-        return setErrorMessage(data.message);
-        
-      }
-
-      setLoading(false);
-
-      if(res.ok){
-        navigate('/');
-
-      }
-
       
+      if (!res.ok) {
+        dispatch(signInFailure(data.message || 'Something went wrong!'));
+        return;
+      }
+
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
-      
-    
   };
 
   return (
@@ -70,7 +62,6 @@ export default function SignIn() {
         {/* right */}
         <div className="flex-1">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-            
             <div>
               <Label value="Your email" />
               <TextInput type="email" placeholder="email" id="email" onChange={handleChange} />
@@ -82,26 +73,19 @@ export default function SignIn() {
             <Button
               className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l text-center"
               type="submit"
-              disabled={isLoading}
+              disabled={loading}
             >
-              {
-                isLoading? (
-                  <>
-
-                    <Spinner size='sm'/>
-                    <span className="p-3">Loading...</span>
-                  
-                  </>
-                  
-
-                ) : 'Sign In'
-              }
-              
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className="p-3">Loading...</span>
+                </>
+              ) : 'Sign In'}
             </Button>
           </form>
           <div className="flex text-sm gap-2 mt-2">
             <span>Dont Have an account?</span>
-            <Link to="/signin" className="text-blue-500 hover:text-blue-600">
+            <Link to="/signup" className="text-blue-500 hover:text-blue-600">
               Sign Up
             </Link>
           </div>
