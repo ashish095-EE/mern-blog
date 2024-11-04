922b56f27670ab211699c6f327f95e752d34ad19
ashish095-EE <127418703+ashish095-EE@users.noreply.github.com>
2024-11-04 19:55:13 +0530

    get post route created with diffrent query setup

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import {verifyToken} from '../utils/verifyUser.js';
-import { create } from '../controllers/post.controller.js';
+import { create, getposts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
-export default router.post('/create', verifyToken,create);
+router.post('/create', verifyToken,create);
+router.get('/getposts',getposts)
+
+
+export default router;
+
+
 
