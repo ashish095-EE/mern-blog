260167265b66498e816ff9536375c1b1cbae644e
ashish095-EE <127418703+ashish095-EE@users.noreply.github.com>
2025-01-02 00:24:38 +0530

    deploy 1

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,26 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-const flowbite = require("flowbite-react/tailwind");
-// eslint-disable-next-line no-undef
-const scrollbar = require('tailwind-scrollbar');
+import flowbite from "flowbite-react/tailwind";
+import scrollbar from "tailwind-scrollbar";
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    // ...
     flowbite.content(),
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    // Add Flowbite plugin
     flowbite.plugin(),
-    
-    // Add Tailwind scrollbar plugin (no method call needed)
     scrollbar,
-
-    
   ],
-}
+};
